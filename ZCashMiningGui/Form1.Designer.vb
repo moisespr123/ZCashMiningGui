ca018dbb2b48c16fb07e24388fdb6a5195c175d6
Moises Cardona <Moises Cardona@DESKTOP-2SB7IHB>
2016-11-02 15:20:49 -0400

    Add project files.

---
+++ ZCashMiningGui/Form1.Designer.vb
@@ -0,0 +1,326 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.AddressField = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.EuropeServer = New System.Windows.Forms.RadioButton()
+        Me.USAServer = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GPUDevicesField = New System.Windows.Forms.TextBox()
+        Me.UseNone = New System.Windows.Forms.RadioButton()
+        Me.UseAMD = New System.Windows.Forms.RadioButton()
+        Me.UseNvidia = New System.Windows.Forms.RadioButton()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Threads = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.UseCPU = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.MinerConsole = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(133, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter your ZCash Address:"
+        '
+        'AddressField
+        '
+        Me.AddressField.Location = New System.Drawing.Point(15, 25)
+        Me.AddressField.Name = "AddressField"
+        Me.AddressField.Size = New System.Drawing.Size(200, 20)
+        Me.AddressField.TabIndex = 2
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.EuropeServer)
+        Me.GroupBox1.Controls.Add(Me.USAServer)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 51)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(139, 49)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Select Server Location:"
+        '
+        'EuropeServer
+        '
+        Me.EuropeServer.AutoSize = True
+        Me.EuropeServer.Location = New System.Drawing.Point(59, 19)
+        Me.EuropeServer.Name = "EuropeServer"
+        Me.EuropeServer.Size = New System.Drawing.Size(59, 17)
+        Me.EuropeServer.TabIndex = 1
+        Me.EuropeServer.TabStop = True
+        Me.EuropeServer.Text = "Europe"
+        Me.EuropeServer.UseVisualStyleBackColor = True
+        '
+        'USAServer
+        '
+        Me.USAServer.AutoSize = True
+        Me.USAServer.Checked = True
+        Me.USAServer.Location = New System.Drawing.Point(6, 19)
+        Me.USAServer.Name = "USAServer"
+        Me.USAServer.Size = New System.Drawing.Size(47, 17)
+        Me.USAServer.TabIndex = 0
+        Me.USAServer.TabStop = True
+        Me.USAServer.Text = "USA"
+        Me.USAServer.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.GPUDevicesField)
+        Me.GroupBox2.Controls.Add(Me.UseNone)
+        Me.GroupBox2.Controls.Add(Me.UseAMD)
+        Me.GroupBox2.Controls.Add(Me.UseNvidia)
+        Me.GroupBox2.Location = New System.Drawing.Point(15, 106)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Use Nvidia or AMD?"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(3, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Device Number:"
+        '
+        'GPUDevicesField
+        '
+        Me.GPUDevicesField.Location = New System.Drawing.Point(6, 65)
+        Me.GPUDevicesField.Name = "GPUDevicesField"
+        Me.GPUDevicesField.Size = New System.Drawing.Size(100, 20)
+        Me.GPUDevicesField.TabIndex = 3
+        Me.GPUDevicesField.Text = "0"
+        '
+        'UseNone
+        '
+        Me.UseNone.AutoSize = True
+        Me.UseNone.Checked = True
+        Me.UseNone.Location = New System.Drawing.Point(122, 19)
+        Me.UseNone.Name = "UseNone"
+        Me.UseNone.Size = New System.Drawing.Size(51, 17)
+        Me.UseNone.TabIndex = 2
+        Me.UseNone.TabStop = True
+        Me.UseNone.Text = "None"
+        Me.UseNone.UseVisualStyleBackColor = True
+        '
+        'UseAMD
+        '
+        Me.UseAMD.AutoSize = True
+        Me.UseAMD.Location = New System.Drawing.Point(67, 19)
+        Me.UseAMD.Name = "UseAMD"
+        Me.UseAMD.Size = New System.Drawing.Size(49, 17)
+        Me.UseAMD.TabIndex = 1
+        Me.UseAMD.TabStop = True
+        Me.UseAMD.Text = "AMD"
+        Me.UseAMD.UseVisualStyleBackColor = True
+        '
+        'UseNvidia
+        '
+        Me.UseNvidia.AutoSize = True
+        Me.UseNvidia.Location = New System.Drawing.Point(6, 19)
+        Me.UseNvidia.Name = "UseNvidia"
+        Me.UseNvidia.Size = New System.Drawing.Size(55, 17)
+        Me.UseNvidia.TabIndex = 0
+        Me.UseNvidia.TabStop = True
+        Me.UseNvidia.Text = "Nvidia"
+        Me.UseNvidia.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Threads)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.UseCPU)
+        Me.GroupBox3.Location = New System.Drawing.Point(15, 212)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox3.TabIndex = 5
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Use CPU?"
+        '
+        'Threads
+        '
+        Me.Threads.Location = New System.Drawing.Point(9, 64)
+        Me.Threads.Name = "Threads"
+        Me.Threads.Size = New System.Drawing.Size(100, 20)
+        Me.Threads.TabIndex = 2
+        Me.Threads.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 48)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Threads:"
+        '
+        'UseCPU
+        '
+        Me.UseCPU.AutoSize = True
+        Me.UseCPU.Location = New System.Drawing.Point(9, 19)
+        Me.UseCPU.Name = "UseCPU"
+        Me.UseCPU.Size = New System.Drawing.Size(44, 17)
+        Me.UseCPU.TabIndex = 0
+        Me.UseCPU.Text = "Yes"
+        Me.UseCPU.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(15, 318)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(200, 66)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Start Mining!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(244, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(172, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Miner Output (May not be realtime):"
+        '
+        'MinerConsole
+        '
+        Me.MinerConsole.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.MinerConsole.Location = New System.Drawing.Point(247, 25)
+        Me.MinerConsole.Multiline = True
+        Me.MinerConsole.Name = "MinerConsole"
+        Me.MinerConsole.Size = New System.Drawing.Size(716, 389)
+        Me.MinerConsole.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(624, 417)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(172, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "GUI developed by Moises Cardona"
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(836, 417)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(127, 13)
+        Me.LinkLabel1.TabIndex = 10
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "http://moisescardona.net"
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "ZCash Miner GUI"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Label6
+        '
+        Me.Label6.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(802, 417)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(28, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "v0.1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(975, 439)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.MinerConsole)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.AddressField)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Nicehash ZCash Miner GUI"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents AddressField As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents EuropeServer As RadioButton
+    Friend WithEvents USAServer As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GPUDevicesField As TextBox
+    Friend WithEvents UseNone As RadioButton
+    Friend WithEvents UseAMD As RadioButton
+    Friend WithEvents UseNvidia As RadioButton
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Threads As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents UseCPU As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents MinerConsole As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Label6 As Label
+End Class
---
+++ ZCashMiningGui/Form1.vb
@@ -0,0 +1,224 @@
+﻿Public Class Form1
+    Private strResults As String
+    Private intStop As Integer
+    Private swReader As System.IO.StreamReader
+    Private thrdCMD As System.Threading.Thread
+    Private Delegate Sub Update()
+    Private uFin As New Update(AddressOf UpdateText)
+    Private Server As String = ""
+    Private Address As String = ""
+    Private UseGPU As Boolean = False
+    Private GPUcommand As String = ""
+    Private GPUDevices As Integer = 0
+    Private CPUThreads As Integer = 0
+
+
+    Private Sub UpdateText()
+        MinerConsole.Text += strResults
+        MinerConsole.SelectionStart = MinerConsole.TextLength - 1
+        MinerConsole.Focus()
+        intStop = MinerConsole.SelectionStart
+        MinerConsole.ScrollToCaret()
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim ValidationError As Boolean = False
+        Dim ErrorMessage As String = "The Following Errors were found:" & vbNewLine
+        If String.IsNullOrEmpty(AddressField.Text) = False Then
+            Address = AddressField.Text
+        Else
+            ValidationError = True
+            ErrorMessage += "-Address field is empty" & vbNewLine
+        End If
+        If USAServer.Checked Then
+            Server = "usa"
+        ElseIf EuropeServer.Checked Then
+            Server = "eu"
+        End If
+        If UseNvidia.Checked Then
+            UseGPU = True
+            GPUcommand = "-cd"
+        ElseIf UseAMD.Checked Then
+            UseGPU = True
+            GPUcommand = "-od"
+        ElseIf UseNone.Checked Then
+            UseGPU = False
+            GPUcommand = "null"
+        End If
+        If UseGPU = True Then
+            If String.IsNullOrEmpty(GPUDevicesField.Text) = False Then
+                If IsNumeric(GPUDevicesField.Text) Then
+                    GPUDevices = GPUDevicesField.Text
+                Else
+                    ValidationError = True
+                    ErrorMessage += "-GPU Devices Number is not numeric" & vbNewLine
+                End If
+            Else
+                ValidationError = True
+                ErrorMessage += "-GPU Devices Number is empty" & vbNewLine
+            End If
+        End If
+        If UseCPU.Checked Then
+            If String.IsNullOrEmpty(Threads.Text) = False Then
+                If IsNumeric(Threads.Text) Then
+                    If Threads.Text = "0" = False Then
+                        CPUThreads = Threads.Text
+                    Else
+                        ValidationError = True
+                        ErrorMessage += "-CPU Threads cannot be zero" & vbNewLine
+                    End If
+                Else
+                    ValidationError = True
+                    ErrorMessage += "-CPU Threads is not numeric" & vbNewLine
+                End If
+            Else
+                ValidationError = True
+                ErrorMessage += "-CPU Threads is empty" & vbNewLine
+            End If
+        End If
+        If UseGPU = False And UseCPU.Checked = False Then
+            ValidationError = True
+            ErrorMessage += "-You must use either the CPU or GPU to mine" & vbNewLine
+        End If
+        If ValidationError = True Then
+            MessageBox.Show(ErrorMessage & "Please fix these errors in order to start mining ZCash")
+        Else
+            Dim writer As New System.IO.StreamWriter("config.conf", False)
+            writer.WriteLine("Address=" & Address)
+            writer.WriteLine("Server=" & Server)
+            writer.WriteLine("GPUCommand=" & GPUcommand)
+            writer.WriteLine("GPUDevices=" & GPUDevices)
+            writer.WriteLine("UseCPU=" & UseCPU.Checked)
+            writer.WriteLine("CPUThreads=" & CPUThreads)
+            writer.Close()
+            thrdCMD = New System.Threading.Thread(AddressOf Miner)
+            thrdCMD.IsBackground = True
+            thrdCMD.Start()
+        End If
+    End Sub
+    Private Sub Miner()
+        Dim ArgumentsToUse As String = ""
+        If UseGPU = True Then
+            If UseCPU.Checked Then
+                ArgumentsToUse = "-l " & Server & " -u " & Address & " -t " & CPUThreads & " " & GPUcommand & " " & GPUDevices
+            Else
+                ArgumentsToUse = "-l " & Server & " -u " & Address & " -t 0 " & GPUcommand & " " & GPUDevices
+            End If
+        Else
+            If UseCPU.Checked Then
+                ArgumentsToUse = "-l " & Server & " -u " & Address & " -t " & CPUThreads
+            Else
+                ArgumentsToUse = "-l " & Server & " -u " & Address & " -t 0 "
+            End If
+        End If
+        Dim procMiner As New Process
+        AddHandler procMiner.OutputDataReceived, AddressOf MinerOutput
+        procMiner.StartInfo.RedirectStandardOutput = True
+        procMiner.StartInfo.CreateNoWindow = True
+        procMiner.StartInfo.UseShellExecute = False
+        procMiner.StartInfo.FileName = System.IO.Directory.GetCurrentDirectory & "\nheqminer\nheqminer_zcash.exe"
+        procMiner.StartInfo.Arguments = ArgumentsToUse
+        procMiner.StartInfo.WorkingDirectory = System.IO.Directory.GetCurrentDirectory & "\nheqminer\"
+        procMiner.Start()
+        procMiner.BeginOutputReadLine()
+        Do Until (procMiner.HasExited)
+            '    'MinerConsole.Text += swReader.Read
+        Loop
+        procMiner.Dispose()
+
+    End Sub
+    Private Sub MinerOutput(ByVal Sender As Object, ByVal OutputLine As DataReceivedEventArgs)
+        strResults = OutputLine.Data & Environment.NewLine
+        Try
+            Invoke(uFin)
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Control.CheckForIllegalCrossThreadCalls = False
+        Dim line As String
+        Dim getdata As String()
+        Dim result As String
+        If My.Computer.FileSystem.FileExists(System.IO.Directory.GetCurrentDirectory & "\nheqminer\nheqminer_zcash.exe") = False Then
+            MessageBox.Show("nheqminer not found. Please download the miner and put it inside a folder called ""nheqminer""")
+            Process.Start("https://github.com/nicehash/nheqminer/releases")
+            Me.Close()
+        End If
+        Try
+            Using reader As New System.IO.StreamReader("config.conf")
+                While Not reader.EndOfStream
+                    line = reader.ReadLine()
+                    If line.Contains("Address") Then
+                        getdata = line.Split("=")
+                        result = getdata(1)
+                        AddressField.Text = result
+                    ElseIf line.Contains("Server") Then
+                        getdata = line.Split("=")
+                        result = getdata(1)
+                        If result = "usa" Then
+                            USAServer.Checked = True
+                        ElseIf result = "eu" Then
+                            EuropeServer.Checked = True
+                        End If
+                    ElseIf line.Contains("GPUCommand") Then
+                        getdata = line.Split("=")
+                        result = getdata(1)
+                        If result = "-od" Then
+                            UseAMD.Checked = True
+                        ElseIf result = "-cd" Then
+                            UseNvidia.Checked = True
+                        Else
+                            UseNone.Checked = True
+                        End If
+                    ElseIf line.Contains("GPUDevices") Then
+                        getdata = line.Split("=")
+                        result = getdata(1)
+                        GPUDevicesField.Text = result
+                    ElseIf line.Contains("UseCPU") Then
+                        getdata = line.Split("=")
+                        result = getdata(1)
+                        If result = "True" Then
+                            UseCPU.Checked = True
+                        Else
+                            UseCPU.Checked = False
+                        End If
+                    ElseIf line.Contains("CPUThreads") Then
+                        getdata = line.Split("=")
+                        result = getdata(1)
+                        Threads.Text = result
+                    End If
+                End While
+            End Using
+        Catch
+        End Try
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        For Each P As Process In System.Diagnostics.Process.GetProcessesByName("nheqminer_zcash")
+            P.Kill()
+        Next
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            NotifyIcon1.Visible = True
+            NotifyIcon1.BalloonTipIcon = ToolTipIcon.Info
+            Me.Hide()
+            NotifyIcon1.BalloonTipTitle = "ZCash Miner GUI"
+            NotifyIcon1.BalloonTipText = "ZCash Miner GUI running on System Tray"
+            NotifyIcon1.ShowBalloonTip(5000)
+        End If
+    End Sub
+    Private Sub NotifyIcon1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles NotifyIcon1.DoubleClick
+        Me.Show()
+        Me.WindowState = FormWindowState.Normal
+        NotifyIcon1.Visible = False
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("http://moisescardona.net")
+    End Sub
+End Class
